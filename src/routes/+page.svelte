f949356de7beccc570954f79036573424c578f49
Angx <norex@qq.com>
2025-02-24 01:07:38 +0800

    首页

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,70 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<main class="page">
+	<h1>拜云开放平台 | Open Palyon</h1>
+
+	<p>敬请期待...</p>
+</main>
+
+<footer class="footer">
+	<p>Copyright©2024-2025 拜云开放平台 All Rights Reserved</p>
+	<p class="ipc">
+		<a href="https://beian.miit.gov.cn/" target="_blank" rel="noopener noreferrer">
+			藏ICP备2023000237号-123
+		</a>
+	</p>
+</footer>
+
+<style lang="less">
+	.page {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		height: calc(100vh - 200px);
+		flex-direction: column;
+		font-size: 14px;
+		font-family:
+			-apply-system,
+			system-ui,
+			ui-sans-serif,
+			-apple-system,
+			BlinkMacSystemFont,
+			'Segoe UI',
+			'Helvetica Neue',
+			Arial,
+			'Noto Sans',
+			sans-serif,
+			'Apple Color Emoji',
+			'Segoe UI Emoji',
+			'Segoe UI Symbol',
+			'Noto Color Emoji';
+
+		h1 {
+			color: #2e6de9;
+			font-size: 40px;
+		}
+
+		p {
+			margin-top: 20px;
+			font-size: 18px;
+			opacity: 0.6;
+		}
+	}
+	.footer {
+		background-color: #f9f9f9;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+		height: 200px;
+		color: #9d9d9d;
+		p {
+			margin: 0;
+		}
+
+		.ipc {
+			margin-top: 20px;
+			a {
+				color: #9d9d9d;
+			}
+		}
+	}
+</style>
